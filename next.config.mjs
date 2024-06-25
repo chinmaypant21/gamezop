9e4bc36f92c4ecd006660af2ead63bdcae689480
Chinmay Pant <64401853+chinmaypant21@users.noreply.github.com>
2024-06-25 12:43:55 +0530

    Setting up PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from "next-pwa";
 
-export default nextConfig;
+const withPWA = nextPWA({
+    dest: "public",
+    // disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+};
+
+export default withPWA(nextConfig);
