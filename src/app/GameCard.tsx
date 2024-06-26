2d2ccc010ba47ff3f80f6379781ebb24662d012d
Chinmay Pant <64401853+chinmaypant21@users.noreply.github.com>
2024-06-26 17:39:46 +0530

    Game card info

--- src/app/GameCard.tsx
+++ src/app/GameCard.tsx
@@ -5,9 +5,28 @@ export default function GameCard({ data }: any) {
     return (
         <div className="relative">
             <button aria-label="Favourite" className="favourite-btn">
-                <Image src={'/icons/favourite_grey.svg'} alt="__" height={30} width={30} className="favourite-img" />
+                <Image src={'/icons/favourite_grey.svg'} alt="Favourite" height={30} width={30} className="favourite-img" />
                 {/* <Image src={'/icons/favourite_red.svg'} alt="__" height={30} width={30} /> */}
             </button>
+            <div className="info-container">
+                <button aria-label="Info" className="info-btn">
+                    <Image src={'/icons/icon.png'} alt="Info Icon" height={30} width={30} className="info-img" />
+                </button>
+
+                <div className="xyz">
+                    <h5 className="info_heading">{data.name.en}</h5>
+                    <div className="info_description">{data.description.en}</div>
+                    <div className="info_gameplay text-sm mt-1"><b>Gameplays:</b> {data.gamePlays}</div>
+                    {
+                        data.gamePreviews.en ? (
+                            <div className="info_game_preview text-sm italic text-blue-500">
+                                <a target="_blank" href={data.gamePreviews.en}>Watch preview</a>
+                            </div>
+                        )
+                        : <div className="text-sm italic text-gray-800">Preview not available</div>
+                    }
+                </div>
+            </div>
             <Link target="_blank" href={data.url}>
                 <Image alt="__" src={data.assets.square} height={190} width={190} className="rounded-2xl w-full" />
                 <h3 className="text-primary font-bold text-lg text-center pt-2">{data.name.en}</h3>
