c4b07ee065fc12be61791f63fb2a791316128ada
Chinmay Pant <64401853+chinmaypant21@users.noreply.github.com>
2024-06-28 12:00:00 +0530

    Restructuring Category page

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,21 +1,52 @@
-import { Suspense, lazy } from 'react';
-import Loading from '@/components/Loading/Loading';
+import { redirect } from 'next/navigation'
 
-const CategoryGames = lazy(() => import('./_CategoryGames/CategoryGames'))
+import Container from '@/components/Container/Container';
+import GameCard from '@/components/GameCard/GameCard';
+import { categories } from "@/data/categories";
 
-export default async function CategoryPage() {
+import styles from './Category.module.css';
+
+interface Params {
+    params: {
+        category: string
+    }
+}
+
+export default async function CategoryPage({ params }: Params) {
     console.log('[+] Fetched in Category Page')
     const response = await fetch('https://pub.gamezop.com/v3/games?id=peSLSV', {
-        next: {revalidate: 60} //Revalidate data every 60 seconds to prevent unnecessary API calls (ISR)
+        next: { revalidate: 60 } //Revalidate data every 60 seconds to prevent unnecessary API calls (ISR)
     });
 
-    const { games } = await response.json();
+    const { games }: { games: Game[] } = await response.json();
+    const selectedCategory = categories.find(cat => cat.id === params.category)
+
+    if (!selectedCategory) {
+        redirect('/')
+    }
+
+    const filteredGames = games.filter(game => (
+        game.categories.en.includes(selectedCategory?.value) ||
+        game.tags.en.includes(selectedCategory?.value)
+    ))
 
     return (
         <main className="flex flex-col items-center justify-between py-4 page-wrapper">
-            <Suspense fallback={<Loading />}>
-                <CategoryGames games={games} />
-            </Suspense>
+            <Container className='flex flex-col gap-3'>
+                <div className="text-2xl text-primary capitalize font-bold">{selectedCategory.name}</div>
+                {
+                    !filteredGames.length ? (
+                        <div className={styles.gameContainer}>
+                            {
+                                filteredGames.map(game => (
+                                    <GameCard key={game.code} data={game} />
+                                ))
+                            }
+                        </div>
+                    )
+                        : <div className='text-dim text-2xl font-bold w-full text-center'>No Games to Show</div>
+                }
+            </Container>
         </main>
     );
 }
